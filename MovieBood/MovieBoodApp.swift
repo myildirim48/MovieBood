995dbf7a4ec47466af5f3b6d29c17846e945e59a
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-03-31 13:50:15 +0300

    Initial Commit

---
+++ MovieBood/MovieBoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoodApp.swift
+//  MovieBood
+//
+//  Created by YILDIRIM on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
